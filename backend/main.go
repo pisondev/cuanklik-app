a9aeaf0f4ee4da27ba2f6844a3c5a5955bf62ac1
Pison Golda <pison.studyroom@gmail.com>
2026-04-02 04:08:51 +0700

    setup folder management & communication file for building app in windows (.exe)

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"strings"
 
 	"github.com/webui-dev/go-webui/v2"
 )
+
+//go:embed frontend
+var frontendFS embed.FS
 
 func main() {
 	InitDB()
@@ -21,11 +26,37 @@ func main() {
 
 	w.SetPort(8080)
 
-	fmt.Println("=====================================================")
-	fmt.Println("successfully run server! open chrome manually:")
-	fmt.Println("http://localhost:8080/frontend/index.html")
-	fmt.Println("=====================================================")
+	w.SetFileHandler(func(filename string) ([]byte, int) {
+		if len(filename) > 0 && filename[0] == '/' {
+			filename = filename[1:]
+		}
 
-	w.Show("frontend/index.html")
+		filepath := "frontend/" + filename
+		data, err := frontendFS.ReadFile(filepath)
+
+		if err != nil {
+			notFound := []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+			return notFound, len(notFound)
+		}
+
+		contentType := "text/plain"
+		if strings.HasSuffix(filename, ".html") {
+			contentType = "text/html"
+		} else if strings.HasSuffix(filename, ".css") {
+			contentType = "text/css"
+		} else if strings.HasSuffix(filename, ".js") {
+			contentType = "application/javascript"
+		}
+
+		header := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: %s\r\n\r\n", contentType)
+		response := append([]byte(header), data...)
+
+		return response, len(response)
+	})
+
+	fmt.Println("Running CuanKLIK Single Executable...")
+
+	w.ShowBrowser("index.html", webui.Chrome)
+
 	webui.Wait()
 }
--- go.mod
+++ go.mod
@@ -7,4 +7,6 @@ require (
 	github.com/webui-dev/go-webui/v2 v2.5.2-0.20260324140909-b79ce073202b
 )
 
-require golang.org/x/crypto v0.49.0 // indirect
+require golang.org/x/crypto v0.49.0
+
+replace github.com/webui-dev/go-webui/v2 => ./modules/go-webui/v2
